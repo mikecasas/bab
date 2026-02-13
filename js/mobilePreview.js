3433e80a2ad078fcf80223a3066d8b83e8dbb373
Mike Casas <mike.casas.public@gmail.com>
2026-02-13 15:15:34 -0500

    Updates.

--- js/mobilePreview.js
+++
@@ -1,9 +0,0 @@
-﻿window.mobilePreview = {
-    set: function (enabled) {
-        if (enabled) {
-            document.body.classList.add("mobile-preview");
-        } else {
-            document.body.classList.remove("mobile-preview");
-        }
-    }
-};
