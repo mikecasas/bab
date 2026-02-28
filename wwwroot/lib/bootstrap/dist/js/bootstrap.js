a676321fb7ecfa7d42b6f3fb2b816fd2c1b31ec9
Casas <mcasas@ppines.com>
2026-02-28 12:11:56 -0500

    Updates.

--- wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -1147,7 +1147,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5080,
+    interval: 5284,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -4317,7 +4317,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5080
+    delay: 5284
   };
 
   /**
